0dc627c03a03a48d7a3d114e496dfac0a89d233c
Azzatoz <83659233+Azzatoz@users.noreply.github.com>
2023-07-11 22:20:01 +0300

    # добавил новую сущность

--- app/src/main/java/com/example/mydiplom_try2/makingYourOwnTraining/TrainingRoomDatabase.kt
+++ app/src/main/java/com/example/mydiplom_try2/makingYourOwnTraining/TrainingRoomDatabase.kt
@@ -9,19 +9,17 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
-@Database(entities = [TrainingRecord::class], version = 1, exportSchema = false)
+@Database(entities = [TrainingRecord::class, TrainingRecordMeta::class], version = 1, exportSchema = false)
 abstract class TrainingRoomDatabase : RoomDatabase() {
 
     abstract fun trainingDao(): TrainingDao
+    abstract fun trainingMetaDao(): TrainingMetaDao
 
     companion object {
         @Volatile
         private var INSTANCE: TrainingRoomDatabase? = null
 
-        fun getDatabase(
-            context: Context,
-            scope: CoroutineScope
-        ): TrainingRoomDatabase {
+        fun getDatabase(context: Context, scope: CoroutineScope): TrainingRoomDatabase {
             return INSTANCE ?: synchronized(this) {
                 val instance = Room.databaseBuilder(
                     context.applicationContext,
@@ -34,16 +32,16 @@ abstract class TrainingRoomDatabase : RoomDatabase() {
                 instance
             }
         }
+    }
 
-        private class TrainingDatabaseCallback(
-            private val scope: CoroutineScope
-        ) : RoomDatabase.Callback() {
-            override fun onCreate(db: SupportSQLiteDatabase) {
-                super.onCreate(db)
-                INSTANCE?.let { database ->
-                    scope.launch(Dispatchers.IO) {
-                        (database.trainingDao())
-                    }
+    private class TrainingDatabaseCallback(
+        private val scope: CoroutineScope
+    ) : RoomDatabase.Callback() {
+        override fun onCreate(db: SupportSQLiteDatabase) {
+            super.onCreate(db)
+            INSTANCE?.let { database ->
+                scope.launch(Dispatchers.IO) {
+                    // Выполните здесь необходимые действия при создании базы данных
                 }
             }
         }
