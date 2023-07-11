fc9cb6a140d1c1c319040cd2801e132f6a9c9655
Azzatoz <83659233+Azzatoz@users.noreply.github.com>
2023-07-11 22:19:07 +0300

    # интерфейс для вывода gameRotationVectorData по тренировкам

--- app/src/main/java/com/example/mydiplom_try2/makingYourOwnTraining/TrainingRecord.kt
+++ app/src/main/java/com/example/mydiplom_try2/makingYourOwnTraining/TrainingRecord.kt
@@ -1,20 +1,15 @@
 package com.example.mydiplom_try2.makingYourOwnTraining
 
-import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 import androidx.room.TypeConverters
 
 @Entity(tableName = "TableTrainingRecord")
 @TypeConverters(FloatListTypeConverter::class)
-
 data class TrainingRecord(
     @PrimaryKey(autoGenerate = true)
     val id: Int = 0,
-    val name_of_training: String,
-    val date: Long,
-    val duration: Long,
-    @ColumnInfo(name = "description")
-    val description: String?,
     val gameRotationVectorData: List<Float>
 )
+
+
