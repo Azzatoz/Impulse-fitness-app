db1043a9a055b0c6281ee40b3a9538add8d5768a
Azzatoz <83659233+Azzatoz@users.noreply.github.com>
2023-08-06 16:42:48 +0300

    # изменил получение и передачу recordName

--- app/src/main/java/com/example/mydiplom_try2/tabs/RecordDetails.kt
+++ app/src/main/java/com/example/mydiplom_try2/tabs/RecordDetails.kt
@@ -1,28 +1,37 @@
 package com.example.mydiplom_try2.tabs
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
 import com.example.mydiplom_try2.R
 import com.example.mydiplom_try2.creatingRecord.MyRoomDatabase
+import com.example.mydiplom_try2.useRecords.Playback
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 
 class RecordDetails : AppCompatActivity() {
+
+    private lateinit var database: MyRoomDatabase
+    private lateinit var recordName: String
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.fragment_record_details)
 
         // Получаем данные о тренировке из интента
-        val recordName = intent.getStringExtra(KEY_RECORD_NAME)!!
+        recordName = intent.getStringExtra("recordName") ?: ""
 
+        // Инициализируем базу данных
+        database = MyRoomDatabase.getDatabase(this@RecordDetails, lifecycleScope, recordName)
         // Выполняем запрос к базе данных для получения записи по имени тренировки
         lifecycleScope.launch {
             val recordMeta = withContext(Dispatchers.IO) {
-                MyRoomDatabase.getDatabase(this@RecordDetails, lifecycleScope, "").metaDao()
-                    .getRecordByName(recordName)
+                database.metaDao().getRecordByName(recordName)
             }
 
             if (recordMeta != null) {
@@ -39,6 +48,16 @@ class RecordDetails : AppCompatActivity() {
                 } else {
                     findViewById<TextView>(R.id.record_description_text_view).text = recordDescription
                 }
+
+                // Находим кнопку start_button и устанавливаем на нее слушатель кликов
+                val startButton = findViewById<Button>(R.id.start_button)
+                startButton.setOnClickListener {
+                    val intent = Intent(this@RecordDetails, Playback::class.java)
+                    intent.putExtra("recordDuration", recordDuration)
+                    intent.putExtra("recordName", recordName)
+                    startActivity(intent)
+                }
+
             }
         }
     }
@@ -48,8 +67,5 @@ class RecordDetails : AppCompatActivity() {
         val seconds = durationInSeconds % 60
 
         return String.format("%02d:%02d:%02d", hours, minutes, seconds)
-    }
-    companion object {
-        private const val KEY_RECORD_NAME = "recordName"
     }
 }
