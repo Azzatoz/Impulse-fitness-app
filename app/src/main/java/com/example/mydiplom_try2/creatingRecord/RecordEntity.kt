077112f56a2c34b082b8059e7a5c3196f313bee0
Azzatoz <83659233+Azzatoz@users.noreply.github.com>
2023-08-06 16:43:06 +0300

    # добавил импорт FloatListTypeConverter

--- app/src/main/java/com/example/mydiplom_try2/creatingRecord/RecordEntity.kt
+++ app/src/main/java/com/example/mydiplom_try2/creatingRecord/RecordEntity.kt
@@ -3,6 +3,7 @@ package com.example.mydiplom_try2.creatingRecord
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 import androidx.room.TypeConverters
+import com.example.mydiplom_try2.additional_files.FloatListTypeConverter
 
 @Entity(tableName = "Record")
 @TypeConverters(FloatListTypeConverter::class)
@@ -11,5 +12,6 @@ data class RecordEntity(
     val id: Int = 0,
     val gameRotationVectorData: List<Float>
 )
+
 
 
