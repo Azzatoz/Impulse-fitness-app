1eba3baf6b92eee5142c6acb055f97a39c52624b
Azzatoz <83659233+Azzatoz@users.noreply.github.com>
2023-08-06 16:39:21 +0300

    # изменил названия, добавил новые параметры

--- app/src/main/java/com/example/mydiplom_try2/additional_files/DatabaseHelper.kt
+++ app/src/main/java/com/example/mydiplom_try2/additional_files/DatabaseHelper.kt
@@ -13,24 +13,29 @@ class DatabaseHelper(private val context: Context) :
 
 
     companion object {
-        private const val DATABASE_NAME = "exercise.db"
+        private const val DATABASE_NAME = "records_statistic.db"
         private const val DATABASE_VERSION = 1
         const val TABLE_STATISTICS = "statistics"
         private const val COLUMN_ID = "id"
         const val COLUMN_DATE = "date"
         const val COLUMN_TIME = "time"
         const val COLUMN_COUNT = "Count"
+        const val COLUMN_NAME = "name" // Добавлено имя записи
+        const val COLUMN_ACCURACY = "accuracy" // Добавлена точность выполнения в процентах
     }
 
     override fun onCreate(db: SQLiteDatabase?) {
         val createTable = "CREATE TABLE $TABLE_STATISTICS (" +
                 "$COLUMN_ID INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                "$COLUMN_NAME TEXT, " +
                 "$COLUMN_DATE TEXT, " +
                 "$COLUMN_TIME TEXT, " +
-                "$COLUMN_COUNT INTEGER)"
+                "$COLUMN_COUNT INTEGER, " +
+                "$COLUMN_ACCURACY INTEGER)"
         db?.execSQL(createTable)
     }
 
+    /** Для удаления записи в меню выбора **/
     fun deleteDatabase(databaseName: String) {
         context.deleteDatabase(databaseName)
     }
@@ -40,10 +45,13 @@ class DatabaseHelper(private val context: Context) :
         val roomDatabaseNames = context.databaseList().filter { it.startsWith("training_db_") }
         for (databaseName in roomDatabaseNames) {
             MyRoomDatabase.deleteDatabase(context, databaseName)
+            context.deleteDatabase(DATABASE_NAME)
         }
-
-        // Удаление базы данных
-        context.deleteDatabase(DATABASE_NAME)
+        // Удаление базы данных exercise.db
+        context.deleteDatabase("exercise.db")
+        context.deleteDatabase("training_database")
+        // Удаление базы данных records_statistic.db
+        context.deleteDatabase("records_statistic.db")
     }
 
 
@@ -53,12 +61,14 @@ class DatabaseHelper(private val context: Context) :
         onCreate(db)
     }
 
-    fun addStatistic(date: String, time: String, count: Int) {
+    fun addStatistic(recordName: String, date: String, time: String, score: Int, accuracy: Double) {
         val db = this.writableDatabase
         val values = ContentValues().apply {
+            put(COLUMN_NAME, recordName)
             put(COLUMN_DATE, date)
             put(COLUMN_TIME, time)
-            put(COLUMN_COUNT, count)
+            put(COLUMN_COUNT, score)
+            put(COLUMN_ACCURACY, accuracy)
         }
         db.insert(TABLE_STATISTICS, null, values)
         db.close()
