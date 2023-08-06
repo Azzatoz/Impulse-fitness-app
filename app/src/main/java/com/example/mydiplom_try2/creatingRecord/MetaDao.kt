52bca13b418b86018205d87efdcf175022a0ec99
Azzatoz <83659233+Azzatoz@users.noreply.github.com>
2023-08-06 16:41:49 +0300

    # изменил комментарии

--- app/src/main/java/com/example/mydiplom_try2/creatingRecord/MetaDao.kt
+++ app/src/main/java/com/example/mydiplom_try2/creatingRecord/MetaDao.kt
@@ -9,44 +9,30 @@ import androidx.room.Update
 
 @Dao
 interface MetaDao {
-    /**
-     * Вставляет новую запись в базу данных.
-     */
+    /** Вставляет новую запись в базу данных.*/
     @Insert(onConflict = OnConflictStrategy.IGNORE)
     fun insert(meta: MetaEntity)
-    /**
-     * Обновляет существующую запись в базе данных.
-     */
+    /** Обновляет существующую запись в базе данных. */
     @Update
     fun update(meta: MetaEntity)
 
-    /**
-     * Получает имя базы данных (ограничено одной записью).
-     */
+    /** Получает имя базы данных (ограничено одной записью). */
     @Query("SELECT name_of_record FROM Meta LIMIT 1")
     fun getDatabaseName(): String?
 
-    /**
-     * Получает все записи из базы данных.
-     */
+    /** Получает все записи из базы данных. */
     @Query("SELECT * FROM Meta")
     fun getAllMeta(): List<MetaEntity>
 
-    /**
-     * Получает имена всех тренировок из базы данных.
-     */
+    /** Получает имена всех тренировок из базы данных. */
     @Query("SELECT name_of_record FROM Meta")
     fun getAllNames(): List<String>
 
-    /**
-     * Получает запись о тренировке по имени тренировки.
-     */
+    /*** Получает запись о тренировке по имени тренировки. */
     @Query("SELECT * FROM Meta WHERE name_of_record = :name")
     fun getRecordByName(name: String): MetaEntity?
 
-    /**
-     * Удаляет запись о тренировке из базы данных.
-     */
+    /**Удаляет запись о тренировке из базы данных. */
     @Delete
     fun delete(training: MetaEntity)
     }
